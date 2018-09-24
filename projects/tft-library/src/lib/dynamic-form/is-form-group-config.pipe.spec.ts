b95ab6707221f5b2cc19f37d0240a313c9af9e71
Nathan Kern - External <nathan.kern@centene.com>
2018-09-24 12:28:59 -0500

    added tests for IsFormGroupConfigPipe

--- projects/tft-library/src/lib/dynamic-form/is-form-group-config.pipe.spec.ts
+++ projects/tft-library/src/lib/dynamic-form/is-form-group-config.pipe.spec.ts
@@ -1,8 +1,34 @@
 import { IsFormGroupConfigPipe } from './is-form-group-config.pipe';
 
-describe('IsFormGroupConfigPipe', () => {
-  it('create an instance', () => {
+fdescribe('IsFormGroupConfigPipe', () => {
+
+  beforeEach( () => {
     const pipe = new IsFormGroupConfigPipe();
-    expect(pipe).toBeTruthy();
+
+    it('should create an instance', () => {
+      expect(pipe).toBeTruthy();
+    });
+
+    it('should return false when passed an array', () => {
+      expect(pipe.transform([])).toBeFalsy();
+    });
+
+    it('should return true when passed a valid config', () => {
+      const validConfig = {
+        'controlType': 'input',
+        'label': 'First name',
+        'inputType': 'text',
+        'controlName': 'firstName',
+        'placeholder': 'Enter your first name',
+        'classes': [],
+        'flexLayoutConfig': {
+          'fxFlex': 40
+        },
+        'validators': [
+          null
+        ]
+      };
+      expect(pipe.transform( validConfig )).toBeTruthy();
+    });
   });
 });
