9068698df35bce0cf0ab0b27048d4f2d8bba452b
Nathan Kern <natjkern@gmail.com>
2018-12-19 21:49:45 -0600

    adds option$ to select field config interface

--- projects/tft-library/src/lib/dynamic-form/form-select/select-field-config.ts
+++ projects/tft-library/src/lib/dynamic-form/form-select/select-field-config.ts
@@ -4,11 +4,10 @@ import { Observable } from 'rxjs';
 export interface SelectFieldConfig extends DynamicFieldConfig {
   options?: SelectOption [];
   optionsCallback?: () => Promise<SelectOption[]>;
-  options$: Observable<any[]>;
+  options$?: Observable<any[]>;
 }
 
 export interface SelectOption {
   label: string;
   value: any;
 }
-
