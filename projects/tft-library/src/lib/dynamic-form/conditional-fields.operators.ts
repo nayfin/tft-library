25bb39afdfda90257e2f36205a61e594ea241d40
David <davidmh.mail@gmail.com>
2019-07-29 22:36:34 -0600

    Add regex matching support for show value strings

--- projects/tft-library/src/lib/dynamic-form/conditional-fields.operators.ts
+++ projects/tft-library/src/lib/dynamic-form/conditional-fields.operators.ts
@@ -3,7 +3,22 @@ import { map } from 'rxjs/operators';
 
 export function valueIn (values: string[]) {
   return map( (value: string) => {
-    return values.includes(value);
+    if (value) {
+      return values.some(v => {
+        if (isRegex(v)) {
+          const trimmedStringForRegex = v.substr(1, v.length - 2);
+          const regex = new RegExp(trimmedStringForRegex);
+          return regex.test(value);
+        } else {
+          return v === value;
+        }
+      });
+    } else {
+      return false;
+    }
   });
 }
 
+function isRegex(string: string) {
+  return string[0] === '/' && string[string.length - 1] === '/';
+}
